124eeb34cc0e761aa74831c4660728b4e420d3a8
WIN-LS06R4L98IU\Administrator <wanziqianhong200711@126.com>
2017-10-23 20:16:38 +0800

    version0.0.5

--- src/game/view/skinPanel/SkinShowComp.ts
+++ src/game/view/skinPanel/SkinShowComp.ts
@@ -4,7 +4,8 @@ class SkinShowComp extends eui.Component {
     private diamondTxt:eui.Label;
     private skinData:any;
     private itemContainer:eui.Group;
-    private itemArr = [];
+    private itemArr:SkinItemRen[] = [];
+    private freeItemArr:SkinItemRen[] = [];
 
     public constructor() {
         super();
@@ -13,14 +14,30 @@ class SkinShowComp extends eui.Component {
 
     public showAllSkin(arr:any) {
         this.skinData = arr;
-        for (let i = 0; i < arr.length; i++) {
-            let comp = new SkinItemRen();
+        while (this.itemArr.length) {
+            let item = this.itemArr.pop();
+            DisplayUtil.removeFromParent(item);
+            this.freeItemArr.push(item);
+        }
+        for (let i = 0, len = arr.length; i < len; i++) {
+            let comp = null;
+            if (this.freeItemArr.length) {
+                comp = this.freeItemArr.pop();
+            } else {
+                comp = new SkinItemRen();
+            }
+            this.itemArr.push(comp);
+            comp.setData(arr[i]);
             this.itemContainer.addChild(comp);
         }
         this.showSomeSkinInfo(arr[0]);
     }
 
     private showSomeSkinInfo(obj:any) {
+        for (let i = 0, len = this.itemArr.length; i < len; i++) {
+            let comp = this.itemArr[i];
+            comp.selected(obj);
+        }
         this.levelTxt.text = Util.getPropValue(obj, "level", "1");
         this.coinTxt.text = Util.getPropValue(obj, "coin", "1");
         this.diamondTxt.text = Util.getPropValue(obj, "diamond", "1");
